f3040457f94d8788f856c19db202487915eacfe8
Yungang <yunganw@amazon.com>
2021-05-29 22:36:21 +1000

    add loading button definition

---
+++ magicLink/vue/src/components/LoadingButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-button variant="success">
+    <span
+      v-if="loading"
+      class="spinner-border spinner-border-sm"
+      role="status"
+      aria-hidden="true"
+    ></span>
+    {{this.loading? "" : this.normalText}}
+  </b-button>
+</template>
+<script>
+export default {
+  name: "LoadingButton",
+  props: {
+    loading: Boolean,
+    normalText: String,
+    loadingText: String
+  },
+}
+</script>
